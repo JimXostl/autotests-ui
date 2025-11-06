91d57f1f6ae27d4db11e14e4918f028ee5d7b364
JimXostl <konstantinskripakov@gmail.com>
2025-11-06 11:41:10 +0300

    Маркировки Pytest

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -2,8 +2,8 @@ import pytest
 from playwright.sync_api import sync_playwright, expect
 
 @pytest.mark.regression
-@pytest.mark.registration
-def test_successful_registration():
+@pytest.mark.courses
+def test_empty_courses_list():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
@@ -12,14 +12,11 @@ def test_successful_registration():
         page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
         email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user@gmail.com')
-
+        email_input.fill('user.name@gmail.com')
         username_input = page.get_by_test_id('registration-form-username-input').locator('input')
         username_input.fill('username')
-
         password_input = page.get_by_test_id('registration-form-password-input').locator('input')
         password_input.fill('password')
-
         registration_button = page.get_by_test_id('registration-page-registration-button')
         registration_button.click()
 
@@ -30,6 +27,12 @@ def test_successful_registration():
         context = browser.new_context(storage_state='browser-state.json')
         page = context.new_page()
 
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-        page.wait_for_timeout(5000)
+        courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+        expect(courses_title).to_be_visible()
+        expect(courses_title).to_have_text('Courses')
+
+        there_is_no_results_text = page.get_by_test_id('courses-list-empty-view-title-text')
+        expect(there_is_no_results_text).to_be_visible()
+        expect(there_is_no_results_text).to_have_text('There is no results')
